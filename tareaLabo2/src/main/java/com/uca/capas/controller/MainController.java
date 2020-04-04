ac23399e8c47a5866f7c9fa1a8f0458324aaff93
RoxanaCortez <00502211@uca.edu.sv>
2020-04-04 16:18:21 -0600

    Contrasena:root usuario:admin

--- tareaLabo2/src/main/java/com/uca/capas/controller/MainController.java
+++ tareaLabo2/src/main/java/com/uca/capas/controller/MainController.java
@@ -13,7 +13,7 @@ public class MainController {
 	public String enviarForm(Usuario usuario) {
 		return "Login";
 	}
-		
+	//usuario admin y contrasena root	
 	@PostMapping("/validacion")
 	public String procesarForm(Usuario usuario) {
 		if(usuario.getUsuario().equals("admin") && usuario.getContra().equals("root")){
